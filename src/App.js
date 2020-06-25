11eb549537e3fbad599b713a275e157632b0678a
archit-m10 <archit_sharma@srmuniv.edu.in>
2020-06-25 16:51:12 +0530

    c

---
+++ src/App.js
@@ -0,0 +1,194 @@
+import React, { useRef, useEffect } from "react";
+import mapboxgl from "mapbox-gl";
+import useSWR from "swr";
+import lookup from "country-code-lookup";
+import "./App.scss";
+// Need mapbox css for tooltips later in the tutorial
+import "mapbox-gl/dist/mapbox-gl.css";
+
+mapboxgl.accessToken =
+  "pk.eyJ1IjoiYXJjaGl0bTEwIiwiYSI6ImNrYmw4dGt2MjA4emcyeHBvMnhwZndpYzcifQ.QfCxsir-585N9agNRgoqqA";
+
+function App() {
+  const mapboxElRef = useRef(null); // DOM element to render map
+
+  const fetcher = url =>
+    fetch(url)
+      .then(r => r.json())
+      .then(data =>
+        data.map((point, index) => ({
+          type: "Feature",
+          geometry: {
+            type: "Point",
+            coordinates: [
+              point.coordinates.longitude,
+              point.coordinates.latitude
+            ]
+          },
+          properties: {
+            id: index,
+            country: point.country,
+            province: point.province,
+            cases: point.stats.confirmed,
+            deaths: point.stats.deaths
+          }
+        }))
+      );
+
+  const { data } = useSWR("https://corona.lmao.ninja/v2/jhucsse", fetcher);
+
+  // Initialize our map
+  useEffect(() => {
+    if (data) {
+      const map = new mapboxgl.Map({
+        container: mapboxElRef.current,
+        style: "mapbox://styles/mapbox/dark-v10",
+        center: [16, 27],
+        zoom: 1
+      });
+
+      // Add navigation controls to the top right of the canvas
+      map.addControl(new mapboxgl.NavigationControl());
+
+      map.once("load", function() {
+        // Add our SOURCE
+        map.addSource("points", {
+          type: "geojson",
+          data: {
+            type: "FeatureCollection",
+            features: data
+          }
+        });
+
+        // Add our layer
+        map.addLayer({
+          id: "circles",
+          source: "points", // this should be the id of source
+          type: "circle",
+          paint: {
+            "circle-opacity": 0.4,
+            "circle-stroke-width": [
+              "interpolate",
+              ["linear"],
+              ["get", "cases"],
+              1,
+              0,
+              100000,
+              0
+            ],
+            "circle-radius": [
+              "interpolate",
+              ["linear"],
+              ["get", "cases"],
+              1,
+              7,
+              1000,
+              11,
+              5000,
+              15,
+              12000,
+              20,
+              25000,
+              30,
+              50000,
+              35,
+              100000,
+              45
+            ],
+            "circle-color": [
+              "interpolate",
+              ["linear"],
+              ["get", "cases"],
+              1,
+              "#ffffb2",
+              5000,
+              "#fed976",
+              10000,
+              "#feb24c",
+              25000,
+              "#fd8d3c",
+              50000,
+              "#FE642E",
+              75000,
+              "#FA5858",
+              100000,
+              "#DF0101"
+            ]
+          }
+        });
+
+        const popup = new mapboxgl.Popup({
+          closeButton: false,
+          closeOnClick: false
+        });
+
+        let lastId;
+
+        map.on("mousemove", "circles", e => {
+          const id = e.features[0].properties.id;
+
+          if (id !== lastId) {
+            lastId = id;
+            const {
+              cases,
+              deaths,
+              country,
+              province
+            } = e.features[0].properties;
+
+            // Change the pointer type on mouseenter
+            map.getCanvas().style.cursor = "pointer";
+
+            const coordinates = e.features[0].geometry.coordinates.slice();
+
+            const countryISO =
+              lookup.byCountry(country)?.iso2 ||
+              lookup.byInternet(country)?.iso2;
+            const provinceHTML =
+              province !== "null" ? `<p>Province: <b>${province}</b></p>` : "";
+            const mortalityRate = ((deaths / cases) * 100).toFixed(2);
+            const countryFlagHTML = Boolean(countryISO)
+              ? `<img src="https://www.countryflags.io/${countryISO}/flat/64.png"></img>`
+              : "";
+
+            const HTML = `<p>Country: <b>${country}</b></p>
+                ${provinceHTML}
+                <p>Cases: <b>${cases}</b></p>
+                <p>Deaths: <b>${deaths}</b></p>
+                <p>Mortality Rate: <b>${mortalityRate}%</b></p>
+                ${countryFlagHTML}`;
+
+            // Ensure that if the map is zoomed out such that multiple
+            // copies of the feature are visible, the popup appears
+            // over the copy being pointed to.
+            while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+              coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+            }
+
+            popup
+              .setLngLat(coordinates)
+              .setHTML(HTML)
+              .addTo(map);
+          }
+        });
+
+        map.on("mouseleave", "circles", function() {
+          lastId = undefined;
+          map.getCanvas().style.cursor = "";
+          popup.remove();
+        });
+      });
+    }
+  }, [data]);
+
+  return (
+    <div className="App">
+      <div className="mapContainer">
+        {/* Mapbox Container */}
+        <div className="mapBox" ref={mapboxElRef} />
+      </div>
+    </div>
+  );
+}
+
+export default App;
